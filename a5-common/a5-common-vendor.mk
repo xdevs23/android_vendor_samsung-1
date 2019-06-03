50edbfdeaab346d094705bc8e6bf3c051a97a671
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-06-03 15:34:24 +0545

    x

--- a5-common/a5-common-vendor.mk
+++ a5-common/a5-common-vendor.mk
@@ -26,7 +26,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a5-common/proprietary/etc/F13QS_imx135_module_info.xml:system/etc/F13QS_imx135_module_info.xml \
     vendor/samsung/a5-common/proprietary/etc/B05QL_s5k5e3yx_module_info.xml:system/etc/B05QL_s5k5e3yx_module_info.xml \
     vendor/samsung/a5-common/proprietary/vendor/lib/hw/camera.vendor.msm8916.so:system/vendor/lib/hw/camera.vendor.msm8916.so \
-    vendor/samsung/a5-common/proprietary/vendor/lib/hw/sensor.vendor.msm8916.so:system/vendor/lib/hw/sensor.vendor.msm8916.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/hw/sensors.vendor.msm8916.so:system/vendor/lib/hw/sensors.vendor.msm8916.so \
     vendor/samsung/a5-common/proprietary/lib/libAl_Awb.so:system/lib/libAl_Awb.so \
     vendor/samsung/a5-common/proprietary/lib/libAl_Awb_Sp.so:system/lib/libAl_Awb_Sp.so \
     vendor/samsung/a5-common/proprietary/lib/libaec_algo_front.so:system/lib/libaec_algo_front.so \
@@ -115,7 +115,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    vendor/samsung/a5-common/proprietary/lib/hw/audio_amplifier.msm8916.so:system/vendor/lib/hw/audio_amplifier.msm8916.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/hw/audio_amplifier.msm8916.so:system/vendor/lib/hw/audio_amplifier.msm8916.so \
     vendor/samsung/a5-common/proprietary/lib/libtfa9895.so:system/lib/libtfa9895.so 
     
     
